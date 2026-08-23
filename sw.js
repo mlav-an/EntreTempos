d2e1abe60c21f372071af80a5fe2b5a8b9ddca0f
mlav-an <martavale1@gmail.com>
2026-08-23 21:49:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    é isso que faz o navegador ir buscar a versão nova.
    ------------------------------------------------------------------ */
 
-const VERSAO = "entretempos-v13";
+const VERSAO = "entretempos-v18";
 
 const FICHEIROS = [
   "./",
